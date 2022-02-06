17a0381469f4bb47f7f112557a08a2e9d268911c
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2022-02-06 12:24:58 +0100

    Create user-routes, controller files

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-// const thoughtRoutes = require("./thought-routes");
-const userRoutes = require("./user-routes");
+const apiRoutes = require("./api");
 
-// router.use("/thoughts", thoughtRoutes);
-router.use("/api/users", userRoutes);
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  return res.send("It's not the correct route.");
+});
 
 module.exports = router;
